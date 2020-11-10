7f1e7466cdf7f83f9f44a7963115db60b017fb46
Andrii Kryvenko <andkryvenko@gmail.com>
2020-11-10 15:50:56 +0200

    оптимизирует код

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 import './style.css';
-import CountdownTimer from './js/CountdownTimer.js';
+import './js/CountdownTimer.js';
 
-const countdownTimer = new CountdownTimer({
-    rootSelector: '#timer-1',
-    daysAtribute: '[data-value="days"]',
-    hoursAtribute: '[data-value="hours"]',
-    minsAtribute: '[data-value="mins"]',
-    secsAtribute: '[data-value="secs"]',
-    targetDate: new Date('Dec 17, 2020'),
-});
-
-const countdownTimer2 = new CountdownTimer({
-    rootSelector: '#timer-2',
-    daysAtribute: '[data-value="days"]',
-    hoursAtribute: '[data-value="hours"]',
-    minsAtribute: '[data-value="mins"]',
-    secsAtribute: '[data-value="secs"]',
-    targetDate: new Date('Dec 17, 2021'),
-});
